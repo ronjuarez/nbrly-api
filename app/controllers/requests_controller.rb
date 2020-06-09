c922886251da02f2d2d670d38e0dd66c561fc423
Jacob1225 <carlone.jacob@gmail.com>
2020-06-09 16:47:25 +0000

    request data formatting

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,12 +1,12 @@
 class RequestsController < ApplicationController
     def index
         @requests = AssistanceRequest.all
-        render json: {status: 'SUCCESS', message: 'loaded ARs', data:@requests},status: :ok
+        render json: {status: 'SUCCESS', message: 'loaded ARs', body:@requests},status: :ok
     end 
 
     def show
         @request = AssistanceRequest.find params[:id]
-        render json: {status: 'SUCCESS', message: 'loaded ARs', data:@request},status: :ok
+        render json: {status: 'SUCCESS', message: 'loaded ARs', body:@request},status: :ok
     end
 
     
@@ -14,7 +14,7 @@ class RequestsController < ApplicationController
         @request = AssistanceRequest.find params[:id]
         @request.destroy
         @requests = AssistanceRequest.all;
-        render json: {status: 'SUCCESS', message: 'AR deleted', data:@requests}, status: :ok
+        render json: {status: 'SUCCESS', message: 'AR deleted', body:@requests}, status: :ok
       end
     
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     def show 
     @user = User.all;
-    render json: {status: 'SUCCESS', message: 'loaded user', data:@user},status: :ok
+    render json: {status: 'SUCCESS', message: 'loaded user', body:@user},status: :ok
     end 
 end
