03dfaf6a8e62f8cc2e617b30d2029aace6c5b36d
Jacob1225 <carlone.jacob@gmail.com>
2020-06-12 04:55:38 +0000

    finalized auth features

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+    include CurrentUserConcern
+
     skip_before_action :verify_authenticity_token
     def create
         user = User.find_by(email: params["user"]["email"]).try(:authenticate, params["user"]["password"])
@@ -14,4 +16,25 @@ class SessionsController < ApplicationController
             render json: { status: 401 }
         end
     end
+
+    def logged_in
+        if @current_user
+            render json: {
+                logged_in: true,
+                user: @current_user
+            }
+        else
+            render json: {
+                logged_in: false
+            }
+    end
+
+    def logout
+        reset_session
+        render json: {
+            status: 200,
+            logged_out: true
+        }
+    end
+
 end
